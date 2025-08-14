df7402c12ec985d0e98815409de897ecabe0a365
Pudimedo <edopudim@gmail.com>
2025-08-14 16:39:12 -0300

    primeira versão teste preciso dar uma estudada bem aprofundada pra aprender melhor

--- static/script/main.js
+++ static/script/main.js
@@ -1,16 +1,22 @@
-function getApiGitHub(){
-    fetch('https://api.github.com/repos/livialop/banquinho')
-    .then( async res =>{
-        if ( !res.ok){
-            throw new Error(res.status)
+async function getTutorial(path) {
+            try {
+                const res = await fetch(`https://api.github.com/repos/livialop/banquinho/contents/${path}`);
+                if (!res.ok) throw new Error(`Erro ao buscar arquivo: ${res.status}`);
+
+                const data = await res.json();
+                const decoded = atob(data.content);
+                return decoded;
+            } catch (err) {
+                console.error(err);
+                return `Erro ao carregar o conteúdo: ${err.message}`;
+            }
         }
 
-        let data = await res.json()
-        console.log(data)
+        async function renderTutorial() {
+            const conteudoMarkdown = await getTutorial("ics/sitenoar.md");
+            const htmlConvertido = marked.parse(conteudoMarkdown);
+            const divo = document.getElementById('divo');
+            divo.innerHTML = htmlConvertido;
+        }
 
-    })
-
-
-}
-
-getApiGitHub()
+        renderTutorial();
