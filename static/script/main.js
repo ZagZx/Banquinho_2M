a9c995db90555ff480761e8a934bbd76a50bab70
Pudimedo <edopudim@gmail.com>
2025-08-20 16:33:27 -0300

    sla

--- static/script/main.js
+++ static/script/main.js
@@ -1,22 +1,22 @@
 async function getTutorial(path) {
-            try {
-                const res = await fetch(`https://api.github.com/repos/livialop/banquinho/contents/${path}`);
-                if (!res.ok) throw new Error(`Erro ao buscar arquivo: ${res.status}`);
+    try {
+        const res = await fetch(`https://api.github.com/repos/livialop/banquinho/contents/${path}`);
+        if (!res.ok) throw new Error(`Erro ao buscar arquivo: ${res.status}`);
 
-                const data = await res.json();
-                const decoded = atob(data.content);
-                return decoded;
-            } catch (err) {
-                console.error(err);
-                return `Erro ao carregar o conteúdo: ${err.message}`;
-            }
-        }
+        const data = await res.json();
+        const decoded = atob(data.content);
+        return decoded;
+    } catch (err) {
+        console.error(err);
+        return `Erro ao carregar o conteúdo: ${err.message}`;
+    }
+}
 
-        async function renderTutorial() {
-            const conteudoMarkdown = await getTutorial("ics/sitenoar.md");
-            const htmlConvertido = marked.parse(conteudoMarkdown);
-            const divo = document.getElementById('divo');
-            divo.innerHTML = htmlConvertido;
-        }
+async function renderTutorial() {
+    const conteudoMarkdown = await getTutorial("ics/sitenoar.md");
+    const htmlConvertido = marked.parse(conteudoMarkdown);
+    const divo = document.getElementById('divo');
+    divo.innerHTML = htmlConvertido;
+}
 
-        renderTutorial();
+renderTutorial();
