0ef2ef60d877c0bc98a132a1d43c8e9932ebbb2f
ZagZx <azevedodvictor@gmail.com>
2025-09-26 15:29:03 -0300

    revertendo commit antigo

--- codigos/sqlite/schema.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS usuarios;
-
-CREATE TABLE usuarios(
-    id INTEGER PRIMARY KEY AUTOINCREMENT, -- em SQLite não tem o underline em AUTO_INCREMENT
-    nome VARCHAR(50),
-    email VARCHAR(255),
-    senha_hash VARCHAR(255)
-)
